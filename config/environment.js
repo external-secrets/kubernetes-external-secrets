ec9b74e99df26a8cdb29abd7189bdb409d92c39e
JacopoDaeli <jacopo.daeli@gmail.com>
2019-04-08 13:10:33 -0400

    chore(eslint): use standard config and fix errors

--- config/environment.js
+++ config/environment.js
@@ -1,30 +1,30 @@
-'use strict';
+'use strict'
 
 /* eslint-disable no-process-env */
 
-const environment = process.env.NODE_ENV ?
-  process.env.NODE_ENV.toLowerCase() : 'development';
+const environment = process.env.NODE_ENV
+  ? process.env.NODE_ENV.toLowerCase() : 'development'
 
 // Validate environment
-const validEnvironments = new Set(['development', 'test', 'production']);
+const validEnvironments = new Set(['development', 'test', 'production'])
 if (!validEnvironments.has(environment)) {
-  throw new Error(`Invalid environment: ${ environment }`);
+  throw new Error(`Invalid environment: ${environment}`)
 }
 
 // Load env file only when development env
 if (environment === 'development') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
 
-const awsRegion = process.env.AWS_REGION || 'us-west-2';
-const eventsIntervalMilliseconds = process.env.EVENTS_INTERVAL_MILLISECONDS ?
-  Number(process.env.EVENTS_INTERVAL_MILLISECONDS) : 60000;
-const pollerIntervalMilliseconds = process.env.POLLER_INTERVAL_MILLISECONDS ?
-  Number(process.env.POLLER_INTERVAL_MILLISECONDS) : 10000;
+const awsRegion = process.env.AWS_REGION || 'us-west-2'
+const eventsIntervalMilliseconds = process.env.EVENTS_INTERVAL_MILLISECONDS
+  ? Number(process.env.EVENTS_INTERVAL_MILLISECONDS) : 60000
+const pollerIntervalMilliseconds = process.env.POLLER_INTERVAL_MILLISECONDS
+  ? Number(process.env.POLLER_INTERVAL_MILLISECONDS) : 10000
 
 module.exports = {
   awsRegion,
   environment,
   eventsIntervalMilliseconds,
   pollerIntervalMilliseconds
-};
+}
--- config/index.js
+++ config/index.js
@@ -1,45 +1,45 @@
-'use strict';
+'use strict'
 
-const AWS = require('aws-sdk');
-const kube = require('kubernetes-client');
-const pino = require('pino');
+const AWS = require('aws-sdk')
+const kube = require('kubernetes-client')
+const pino = require('pino')
 
-const envConfig = require('./environment');
-const CustomResourceManager = require('../lib/custom-resource-manager');
-const customResourceManifest = require('../custom-resource-manifest.json');
-const SecretsManagerBackend = require('../lib/backends/secrets-manager-backend');
-const SystemManagerBackend = require('../lib/backends/system-manager-backend');
+const envConfig = require('./environment')
+const CustomResourceManager = require('../lib/custom-resource-manager')
+const customResourceManifest = require('../custom-resource-manifest.json')
+const SecretsManagerBackend = require('../lib/backends/secrets-manager-backend')
+const SystemManagerBackend = require('../lib/backends/system-manager-backend')
 
-let kubeClientConfig;
+let kubeClientConfig
 try {
-  kubeClientConfig = kube.config.getInCluster();
+  kubeClientConfig = kube.config.getInCluster()
 } catch (err) {
-  kubeClientConfig = kube.config.fromKubeconfig();
+  kubeClientConfig = kube.config.fromKubeconfig()
 }
-const kubeClient = new kube.Client({ config: kubeClientConfig });
+const kubeClient = new kube.Client({ config: kubeClientConfig })
 
 const logger = pino({
   serializers: {
     err: pino.stdSerializers.err
   }
-});
+})
 
 const customResourceManager = new CustomResourceManager({
   kubeClient,
   logger
-});
+})
 
-const secretsManagerClient = new AWS.SecretsManager({ region: envConfig.awsRegion });
-const secretsManagerBackend = new SecretsManagerBackend({ client: secretsManagerClient, logger });
-const systemManagerClient = new AWS.SSM({ region: envConfig.awsRegion });
-const systemManagerBackend = new SystemManagerBackend({ client: systemManagerClient, logger });
+const secretsManagerClient = new AWS.SecretsManager({ region: envConfig.awsRegion })
+const secretsManagerBackend = new SecretsManagerBackend({ client: secretsManagerClient, logger })
+const systemManagerClient = new AWS.SSM({ region: envConfig.awsRegion })
+const systemManagerBackend = new SystemManagerBackend({ client: systemManagerClient, logger })
 const backends = {
   secretsManager: secretsManagerBackend,
   systemManager: systemManagerBackend
-};
+}
 
 // backwards compatibility
-backends.secretManager = secretsManagerBackend;
+backends.secretManager = secretsManagerBackend
 
 module.exports = {
   backends,
@@ -48,4 +48,4 @@ module.exports = {
   ...envConfig,
   kubeClient,
   logger
-};
+}
