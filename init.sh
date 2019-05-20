8d450bc11596d1ef17f640ae222f9af6c98f2982
JacopoDaeli <jacopo.daeli@gmail.com>
2019-05-20 17:16:49 -0400

    add if AWS 2

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-LOCAL_HOSTNAME=$(hostname -d)
+LOCAL_HOSTNAME=$(hostname)
 
 if [[ ${LOCAL_HOSTNAME} =~ .*\.amazonaws\.com ]]; then
   EC2_AVAIL_ZONE=`wget -q -O- http://169.254.169.254/latest/meta-data/placement/availability-zone || true`
