1c472036ff876906ecc1cdaac61fc1edb66f9497
JacopoDaeli <jacopo.daeli@gmail.com>
2019-05-20 17:15:13 -0400

    add if AWS

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# Use EC2 instance metadata to get the region https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
-EC2_AVAIL_ZONE=`wget -q -O- http://169.254.169.254/latest/meta-data/placement/availability-zone || true`
-export AWS_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+LOCAL_HOSTNAME=$(hostname -d)
+
+if [[ ${LOCAL_HOSTNAME} =~ .*\.amazonaws\.com ]]; then
+  EC2_AVAIL_ZONE=`wget -q -O- http://169.254.169.254/latest/meta-data/placement/availability-zone || true`
+  export AWS_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+fi
