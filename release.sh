af43aa1f7e758300a8d6e7b2e0bc56bbf01581a1
Silas Boyd-Wickizer <silasbw@gmail.com>
2019-06-03 14:26:02 -0700

    chore(release): fix release.sh to work with charts

--- release.sh
+++ release.sh
@@ -8,8 +8,8 @@ TAG=$(git describe)
 docker build -t godaddy/kubernetes-external-secrets:$SHA .
 docker tag godaddy/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:$TAG
 
-sed -i "s/godaddy\/kubernetes-external-secrets:[a-zA-Z0-9\.]*/godaddy\/kubernetes-external-secrets:$TAG/" external-secrets.yml
-git commit external-secrets.yml -m "chore(release): godaddy/kubernetes-external-secrets:$TAG"
+sed -i "s/tag: [a-zA-Z0-9\.]*/tag: $TAG/" charts/kubernetes-external-secrets/values.yaml
+git commit charts/kubernetes-external-secrets/values.yaml -m "chore(release): godaddy/kubernetes-external-secrets:$TAG"
 
 echo ""
 echo "Run the following to publish:"
