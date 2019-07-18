2b6aff88f7d6d049578bb0f4ac1d618b7d89ce46
Silas Boyd-Wickizer <silasbw@gmail.com>
2019-07-17 17:16:17 -0700

    chore(release): include a :latest tag with releases

--- release.sh
+++ release.sh
@@ -7,6 +7,7 @@ TAG=$(git describe)
 
 docker build -t godaddy/kubernetes-external-secrets:$SHA .
 docker tag godaddy/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:$TAG
+docker tag godaddy/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:latest
 
 sed -i "s/tag: [a-zA-Z0-9\.]*/tag: $TAG/" charts/kubernetes-external-secrets/values.yaml
 git commit charts/kubernetes-external-secrets/values.yaml -m "chore(release): godaddy/kubernetes-external-secrets:$TAG"
@@ -14,5 +15,5 @@ git commit charts/kubernetes-external-secrets/values.yaml -m "chore(release): go
 echo ""
 echo "Run the following to publish:"
 echo ""
-echo "  git push --follow-tags origin master && docker push godaddy/kubernetes-external-secrets:$TAG"
+echo "  git push --follow-tags origin master && docker push godaddy/kubernetes-external-secrets:$TAG && docker push godaddy/kubernetes-external-secrets:latest"
 echo ""
