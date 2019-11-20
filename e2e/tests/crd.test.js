f8a35b60cb35215f9d333855ca34b989eebe95c4
Silas Boyd-Wickizer <silasbw@gmail.com>
2019-11-20 10:16:14 -0800

    Lint fixes

--- e2e/tests/crd.test.js
+++ e2e/tests/crd.test.js
@@ -27,22 +27,24 @@ describe('CRD', () => {
     kubeClient
       .apis[customResourceManifest.spec.group]
       .v1.namespaces('default')[customResourceManifest.spec.names.plural]
-      .post({ body: {
-        apiVersion: 'kubernetes-client.io/v1',
-        kind: 'ExternalSecret',
-        metadata: {
-          name: `e2e-test-validation-${uuid}`
-        },
-        secretDescriptor: {
-          backendType: 'systemManager',
-          data: [
-            {
-              key: `/e2e/${uuid}/name`,
-              name: 'name'
-            }
-          ]
+      .post({
+        body: {
+          apiVersion: 'kubernetes-client.io/v1',
+          kind: 'ExternalSecret',
+          metadata: {
+            name: `e2e-test-validation-${uuid}`
+          },
+          secretDescriptor: {
+            backendType: 'systemManager',
+            data: [
+              {
+                key: `/e2e/${uuid}/name`,
+                name: 'name'
+              }
+            ]
+          }
         }
-      } })
+      })
       .catch(err => expect(err).to.be.an('error'))
   })
 })
