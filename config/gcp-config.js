ed7e736c30bab0aff8e563e740c3f5939827b9da
Karan <karan.kapoor@aeris.net>
2020-02-24 17:18:08 +0530

    Changes for supporting GCP Secret Manager

--- config/gcp-config.js
+++ config/gcp-config.js
@@ -5,10 +5,16 @@ const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
 module.exports = {
   gcpSecretManager: () => {
 
-	// use the service account json file for authentication & authorization
-	// TODO: how to bootstrap 
-  	const options = {
-		keyFilename: 'key-name-can-come-from-env',
+	// Reads the secret object with the json file and creates the option object
+	// only sa email address and private key are needed, project id is for fetching 
+	// secret
+	const serviceAccountDetails = JSON.parse(process.env.GCP_SERVICE_ACCOUNT_JSON);
+	const options = {
+		credentials : {
+			client_email: serviceAccountDetails.client_email,
+			private_key: serviceAccountDetails.private_key
+		},
+		projectId: serviceAccountDetails.project_id
 	}
 
     const gcpClient = new SecretManagerServiceClient(options);
