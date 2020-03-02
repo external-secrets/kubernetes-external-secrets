d1d5d53686d30c7e14a8b22a780559d00c67ade5
Karan <karan.kapoor@aeris.net>
2020-03-02 11:33:15 +0530

    Create GCP Secret Manager Client using env variable GOOGLE_APPLICATION_CREDENTIALS

--- config/gcp-config.js
+++ config/gcp-config.js
@@ -4,20 +4,9 @@ const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
 
 module.exports = {
   gcpSecretManager: () => {
-
-	// Reads the secret object with the json file and creates the option object
-	// only sa email address and private key are needed, project id is for fetching 
-	// secret
-	const serviceAccountDetails = JSON.parse(process.env.GCP_SERVICE_ACCOUNT_JSON);
-	const options = {
-		credentials : {
-			client_email: serviceAccountDetails.client_email,
-			private_key: serviceAccountDetails.private_key
-		},
-		projectId: serviceAccountDetails.project_id
-	}
-
-	const gcpClient = new SecretManagerServiceClient(options);
+  	// Reads the environment variable GOOGLE_APPLICATION_CREDENTIALS to load the service
+  	// account credentials
+	const gcpClient = new SecretManagerServiceClient();
 	return gcpClient
   }
 }
