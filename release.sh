eebf30e22a0eed7e8b0258ad9d7cd0f17d06cf59
Markus Maga <markus@maga.se>
2020-12-22 03:27:54 +0100

    chore: migration godaddy -> external-secrets renames (#587)

--- release.sh
+++ release.sh
@@ -3,13 +3,13 @@
 set -e
 
 if [ -z "$ALLOW_DIRTY" ]; then
-    if ! output=$(git status --porcelain) || ! [ -z "$output" ]; then
-        git status
-        echo ""
-        echo "Ensure working directory is clean before releasing."
-        echo ""
-        exit 1
-    fi
+	if ! output=$(git status --porcelain) || ! [ -z "$output" ]; then
+		git status
+		echo ""
+		echo "Ensure working directory is clean before releasing."
+		echo ""
+		exit 1
+	fi
 fi
 
 SHA=$(git rev-parse --short HEAD)
@@ -21,14 +21,14 @@ perl -i -pe "s/appVersion: [a-zA-Z0-9\.]*/appVersion: $TAG/" charts/kubernetes-e
 perl -i -pe "s/version: [a-zA-Z0-9\.]*/version: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
 (cd charts/kubernetes-external-secrets && helm package . && helm repo index --merge ../../docs/index.yaml ./ && mv *.tgz ../../docs && mv index.yaml ../../docs)
 
-docker build -t godaddy/kubernetes-external-secrets:$SHA .
-docker tag godaddy/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:$TAG
-docker tag godaddy/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:latest
+docker build -t ghcr.io/external-secrets/kubernetes-external-secrets:$SHA .
+docker tag ghcr.io/external-secrets/kubernetes-external-secrets:$SHA ghcr.io/external-secrets/kubernetes-external-secrets:$TAG
+docker tag ghcr.io/external-secrets/kubernetes-external-secrets:$SHA ghcr.io/external-secrets/kubernetes-external-secrets:latest
 
 echo ""
 echo "Do the following to publish:"
 echo ""
 echo "  1. inspect local changes (e.g., git status, git diff)"
-echo "  2. git add --all && git commit -m \"chore(release): godaddy/kubernetes-external-secrets:$TAG\""
-echo "  3. git push --follow-tags origin master && docker push godaddy/kubernetes-external-secrets:$TAG && docker push godaddy/kubernetes-external-secrets:latest"
+echo "  2. git add --all && git commit -m \"chore(release): external-secrets/kubernetes-external-secrets:$TAG\""
+echo "  3. git push --follow-tags origin master && docker push ghcr.io/external-secrets/kubernetes-external-secrets:$TAG && docker push ghcr.io/external-secrets/kubernetes-external-secrets:latest"
 echo ""
