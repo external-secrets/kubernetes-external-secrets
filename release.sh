472ad251cc1f4ffc46c462221ee2956316f9d186
Markus Maga <markus@maga.se>
2021-03-18 04:49:20 +0100

    fix: remove instructions to push docker image when cutting release
    Signed-off-by: Markus Maga <markus@maga.se>

--- release.sh
+++ release.sh
@@ -21,14 +21,10 @@ perl -i -pe "s/appVersion: [a-zA-Z0-9\.]*/appVersion: $TAG/" charts/kubernetes-e
 perl -i -pe "s/version: [a-zA-Z0-9\.]*/version: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
 (cd charts/kubernetes-external-secrets && helm package . && helm repo index --merge ../../docs/index.yaml ./ && mv *.tgz ../../docs && mv index.yaml ../../docs)
 
-docker build -t ghcr.io/external-secrets/kubernetes-external-secrets:$SHA .
-docker tag ghcr.io/external-secrets/kubernetes-external-secrets:$SHA ghcr.io/external-secrets/kubernetes-external-secrets:$TAG
-docker tag ghcr.io/external-secrets/kubernetes-external-secrets:$SHA ghcr.io/external-secrets/kubernetes-external-secrets:latest
-
 echo ""
 echo "Do the following to publish:"
 echo ""
 echo "  1. inspect local changes (e.g., git status, git diff)"
 echo "  2. git add --all && git commit -m \"chore(release): external-secrets/kubernetes-external-secrets:$TAG\""
-echo "  3. git push --follow-tags origin master && docker push ghcr.io/external-secrets/kubernetes-external-secrets:$TAG && docker push ghcr.io/external-secrets/kubernetes-external-secrets:latest"
+echo "  3. git push --follow-tags origin master"
 echo ""
